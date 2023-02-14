d5e8ab66c3bfb5725f683f3cf7deb07e9ec81d45
UJustBe Tech <88655561+ujustbetech@users.noreply.github.com>
2023-02-14 15:47:14 +0530

    login method get

--- src/component/Login.js
+++ src/component/Login.js
@@ -11,13 +11,12 @@ function Login() {
     const [mobilenumber, setmobilenumber] = useState('');
 
 
-    // const config = {
-    //     headers: {
-    //         'content-type': 'application/json',
-    //         'Access-Control-Allow-Origin': '*',
-    //         'Access-Control-Allow-Headers': '*'
-    //     }
-    // }
+    const config = {
+        headers: {
+            'content-type': 'application/json',
+            'Access-Control-Allow-Origin': "*"
+        }
+    }
 
     const dataToBeFedToFootballersAPI = {
         firstname: firstname,
@@ -26,48 +25,20 @@ function Login() {
         mobilenumber: mobilenumber,
     }
 
-    var data = JSON.stringify({
-        firstname: firstname,
-        lastname: lastname,
-        role: role,
-        mobilenumber: mobilenumber,
-      });
-      
-      var config = {
-        method: 'post',
-      maxBodyLength: Infinity,
-        url: 'https://sandhiyaacharya.pythonanywhere.com/reg/',
-        headers: { 
-          'Content-Type': 'application/json'
-        },
-        data : data
-      };
-      
-     
-
     const handleClick = async (event) => {
         event.preventDefault()
 
-        // await axios.post(`https://sandhiyaacharya.pythonanywhere.com/reg/`, dataToBeFedToFootballersAPI, config)
-        //     .then((response) => {
-        //         // dispatch(createFootballPlayerProfile(response.data))
-        //         console.log(response.data)
-        //         // setPostValStatus(true);
-        //     })
+        await axios.post(`https://sandhiyaacharya.pythonanywhere.com/reg/`, dataToBeFedToFootballersAPI, config)
+            // .then((response) => {
+            //     // dispatch(createFootballPlayerProfile(response.data))
+            //     console.log(response.data)
+            //     // setPostValStatus(true);
+            // })
 
-       await axios(config)
-        .then(function (response) {
-          console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-        
-
-//             await axios.get("https://sandhiyaacharya.pythonanywhere.com/reg/")
-//   .then(function (response) {
-//     console.log(response);
-//   });
+           await axios.get("https://finalspaceapi.com/api/v0/character/?limit=2")
+  .then(function (response) {
+    console.log(response);
+  });
     }
 
 
