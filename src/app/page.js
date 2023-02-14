a1e259690600477f5b0027ebe9f68f4170de934e
UJustBe Tech <88655561+ujustbetech@users.noreply.github.com>
2023-02-14 12:43:54 +0530

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+// import Image from 'next/image'
+// import { Inter } from '@next/font/google'
+// import styles from './page.module.css'
+// import Dashboard from '../components/Dashboard'
+// import Login from '../components/Login'
+// import styles from '../styles/main.scss'
+
+
+// const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      {/* <Login/> */}
+      <h1>anniversary page</h1>
+      <h6>Content</h6>
+    </>
+  )
+}
